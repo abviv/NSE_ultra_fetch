bb51215efc0a42c9d289dbc3ccf8afaad78ab0b3
Abishek <abishek.v.a@gmail.com>
2017-06-27 17:26:31 +0200

    server update
    Removed a bug which prevented the proper functioning on a server.

--- variant_anubis/princess_leia.py
+++ variant_anubis/princess_leia.py
@@ -1,5 +1,8 @@
-"""__main__production__code   Princess Leia, i have no idea why i named it. Pulls the data from NSE which is EOD data.
-				------------------pulls  data------------------ 
+"""__main__production__code   Princess Leia, i have no idea why i named it. Following code pulls the data from NSE, EOD data.
+
+--------------->Run only this file
+---------------->pulls  data
+
 """
 
 import datetime
@@ -10,24 +13,28 @@ import os, zipfile,sys,shutil
 from os import listdir
 from os.path import isfile, join
 from datetime import datetime
-from anubis_main import *      
+from anubis_main import * 
 
 file_path = os.getcwd()
 directory_to_bhav = file_path + '/BHAV_CPY/'					
 directory_to_oi = file_path +'/OI/' 				
 directory_to_vol = file_path + '/VOLATILITY/'
 
-day=datetime.now().date().strftime ("%d")         #.strftime ("%Y%m%d") ###for fetching the current date and strips the needed 
-month=datetime.now().date().strftime ("%m")
+#day=datetime.now().date().strftime ("%d")         #.strftime ("%Y%m%d") ###for fetching the current date and strips the needed 
+#month=datetime.now().date().strftime ("%m")
+#print "current day--->" + day
+#print "current month->" + month
 
 
 list_of_months = ['unindex','JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
-month_in_words = list_of_months[int(month)]
+#print month
+#month_in_words = list_of_months[int(month)]
+#print "current month--->" + month_in_words
 
 
 def Bhav_Cpy(month_var,day_var):
-	month_temp = month_var            #str
-	day_temp = day_var		  #int
+	month_temp = month_var          		  #str
+	day_temp = day_var				  #int
 	
 	def Bhav_Cpy_mental():
 			if (day_temp<=9):
@@ -51,14 +58,16 @@ def Bhav_Cpy(month_var,day_var):
 
 	if not os.path.exists(directory_to_bhav):
 		os.mkdir(directory_to_bhav)
+		
 		Bhav_Cpy_mental()
+	
 	else:
 		print "ELse part"
 		Bhav_Cpy_mental()
 
 
 def Open_Interest(month_in_num_var,day_var):
-	month_temp = month_in_num_var     		  #int
+	month_temp = month_in_num_var    		  #int
 	day_temp = day_var				  #int
 	
 	def Open_Interest_mental():
@@ -84,14 +93,16 @@ def Open_Interest(month_in_num_var,day_var):
 
 	if not os.path.exists(directory_to_oi):
 		os.mkdir(directory_to_oi)
+		
 		Open_Interest_mental()
+	
 	else:
 		print "ELse part"
 		Open_Interest_mental()	
 
 
 def Volatility(month_var,day_var):
-	month_temp = month_var           		  #str
+	month_temp = month_var            		  #str
 	day_temp = day_var				  #int
 	
 	def Volatility_mental():
@@ -118,7 +129,9 @@ def Volatility(month_var,day_var):
 
 	if not os.path.exists(directory_to_vol):
 		os.mkdir(directory_to_vol)
+		
 		Volatility_mental()
+	
 	else:
 		print "ELse part"
 		Volatility_mental()
@@ -127,13 +140,14 @@ def Volatility(month_var,day_var):
 
 def Unzipper(unzip_directory):
 	file_path=os.getcwd()
-	
 	###listdir is used to list the contents of a directory
 	onlyfiles = os.listdir(unzip_directory)
-
+	#print (unzip_directory)
+	#print (onlyfiles)
 	for i in onlyfiles:
+	#if o.endswith(".zip"):
 		path_to_zipfile= unzip_directory + "/{}".format(i)
-		
+		#print (path_to_zipfile)
 		###refer zipfile doc for more info about the underlying function
 		zip = zipfile.ZipFile(path_to_zipfile, 'r')
 		zip.extractall(unzip_directory)
@@ -142,25 +156,32 @@ def Unzipper(unzip_directory):
 
 
 def Full_Clean():
-	directory =[directory_to_bhav,directory_to_vol,directory_to_oi] 
-	
+	directory =[directory_to_bhav,directory_to_vol,directory_to_oi]  		#directory_to_bhav,directory_to_vol,
+	#print directory[0]
 	for i in range(len(directory)):
 	    test = os.listdir( directory[i] )
+	    #print test
 	    removable_file = (".zip",".xml")
-		
 	    for item in test:
+	        #print item
 	        if item.endswith(removable_file):
 	            os.remove(os.path.join(directory[i], item ))
 
-
 ###custom function for pulling particular date
-def Groupping():									
+def Groupping():
+
+	day=datetime.now().date().strftime ("%d")         #.strftime ("%Y%m%d") ###for fetching the current date and strips the needed 
+	month=datetime.now().date().strftime ("%m")									
+	month_in_words = list_of_months[int(month)]									
+	
 	Bhav_Cpy(month_in_words,day)			#(month,day) ("str(first three letter of the month)",int)
 	Unzipper (directory_to_bhav)
 	Open_Interest(month,day)			#(month,day) (int,int)
 	Unzipper(directory_to_oi)     
 	Volatility(month,day)				#(month,day)  (int,int)
 	Full_Clean()
+
+
 
 ###contains the link defn to all the proper flow  ****do not modify **** 
 def Godzilla():										
@@ -173,28 +194,31 @@ def Godzilla():
 		shutil.rmtree(directory_to_oi)
 		print "=====Running Princess Leia====="
 		Groupping()	
+		
 
 ###SCHEDULER CALLS ALL THIS FUNCTION AT THE PREDEFINED TIME https://schedule.readthedocs.io/en/stable/
 
-schedule.every().monday.at("17:29").do(Godzilla)		###for Indian users change time to 19:29
-schedule.every().monday.at("17:30").do(Bitch_Pls_Trigger)	###for Indian users change time to 19:30 
 
-schedule.every().tuesday.at("17:29").do(Godzilla)
-schedule.every().tuesday.at("17:30").do(Bitch_Pls_Trigger)
+schedule.every().day.at("15:00").do(Godzilla)
+schedule.every().day.at("15:01").do(Bitch_Pls_Trigger)
 
-schedule.every().wednesday.at("17:29").do(Godzilla)
-schedule.every().wednesday.at("17:30").do(Bitch_Pls_Trigger)
+#schedule.every().wedensday.at("22:18").do(Godzilla)               
+#schedule.every().wednesday.at("22:19").do(Bitch_Pls_Trigger)	
 
-schedule.every().thursday.at("17:29").do(Godzilla)
-schedule.every().thursday.at("17:30").do(Bitch_Pls_Trigger)
 
-schedule.every().friday.at("17:29").do(Godzilla)
-schedule.every().friday.at("17:30").do(Bitch_Pls_Trigger)
-
-#schedule.every().day.at("22:18").do(Godzilla)
-#schedule.every().day.at("22:19").do(Bitch_Pls_Trigger)	
-
-if __name__ == '__main__':
+###DISABLE THIS TO STOP THE WHOLE PROGRAM
+if __name__ == '__main__':    
 	while True:
-		schedule.run_pending()
-		time.sleep(1)
+		if datetime.today().weekday() == 0:
+			schedule.run_pending()
+		elif datetime.today().weekday() == 1:
+			schedule.run_pending()
+		elif datetime.today().weekday() == 2:
+			schedule.run_pending()
+		elif datetime.today().weekday() == 3:
+			schedule.run_pending()
+		elif datetime.today().weekday() == 4:
+			schedule.run_pending()
+		else:
+			pass
+	time.sleep(2)
